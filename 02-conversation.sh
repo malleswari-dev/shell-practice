d9d6acdf99491cf0c8a1eabc7792e425847690ef
malleswari <maddikirimalleswari4@gmail.com>
2025-09-20 17:35:41 +0530

    conversations

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo hii friends
+echo "ramesh :: hi suresh, how are you?"
+echo "suresh :: hello ramesh, i am fine . how are you?"
+echo "ramesh :: what are you learning recently?"
+echo "suresh :: i am learning shell scripting ramesh"
