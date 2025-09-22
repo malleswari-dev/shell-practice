577c1cb4c9e7a7823539f31b7a86c0d55892fc66
malleswari <maddikirimalleswari4@gmail.com>
2025-09-22 12:06:26 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,4 +7,4 @@ PERSON2=$2
 echo "$PERSON1 :: welcome to alaska $PERSON2"
 echo "$PERSON2 :: thankyou $PERSON1, finally not on amazon webinar"
 echo "$PERSON1 :: so do we have a deal"
-echo $PERSON2 :: yes , if you make ukraine joins russia"
+echo "$PERSON2 :: yes , if you make ukraine joins russia"
