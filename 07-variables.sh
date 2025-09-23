3273e65d64dc5768cb5a88974dbc549b522dd711
malleswari <maddikirimalleswari4@gmail.com>
2025-09-23 12:06:54 +0530

    variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -4,7 +4,7 @@ DATE=$(date)
 echo time stamp executed: $DATE
 
 start_time=$(date +%s)
-sleep 10 &
+sleep 10 
 end_time=$(date +%s)
 total_time=$(($end_time-$start_time))
 echo "script executed in : $total_time seconds"
