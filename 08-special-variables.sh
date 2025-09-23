567d18e13872b6467992f7e6f180dfd24d1c741d
malleswari <maddikirimalleswari4@gmail.com>
2025-09-23 12:13:42 +0530

    variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,9 @@
 
 echo "all variables passed to the script: $@"
 echo "all variables passed to the script: $*"
+echo "script name:$0"
+echo "first argument:$1"
+echo "current directory:$pwd"
+echo "who is running this:$user"
+echo "home directory of user:$home"
+echo "PID of the script:$$"
