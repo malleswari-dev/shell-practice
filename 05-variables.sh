7c72d26788e8e482ed57d5a7f043009eb2ff7b5d
malleswari <maddikirimalleswari4@gmail.com>
2025-09-23 11:45:16 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,3 +3,8 @@
 echo "please enter your pin number"
 read -s PIN_NUMBER # here pin_number is variable
 echo "pin entered is : $PIN_NUMBER"
+
+#!/bin/bash
+echo "malleswari:"
+read name
+echo "Hello, $name!"
