c6918549fb64c6029936d1705b7f64fab176268c
malleswari <maddikirimalleswari4@gmail.com>
2025-09-23 12:30:31 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -3,5 +3,17 @@
 NUMBER1=100
 NUMBER2=200
 NAME=devops
-SUM=$(($NUMBER1+$NUMBER2+$NAME))
+
+SUM=$(($NUMBER1+$NUMBER2))
 echo "SUM is:${SUM}"
+
+# SUM=$(($NUMBER1+$NUMBER2+$NAME))
+# echo "SUM is:${SUM}"
+
+#size =4,max index = 3
+LEADERS=("modi" "putin" "trudo" "trump")
+
+echo "all leaders:${LEADERS[@]}"
+echo "first leader:${LEADERS[0]}"
+echo "second leader:${LEADERS[1]}"
+echo "first leader:${LEADERS[10]}"
